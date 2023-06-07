54c8583d6ed2bbeeb7a2e0f01626880ffb00bfb8
sarfrazali78 <sarfrazalikamleu@gmail.com>
2023-06-07 12:57:45 +0530

    description and navbar

--- kanban-project-master/src/App.js
+++ kanban-project-master/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './components/atoms/atoms/navbar/Navbar';
-
+import Icons from './components/atoms/molecules/Icons';
+import './App.css';
 function App() {
   return (
     <div className="App">
        <Navbar/>
+       <Icons/>
     </div>
   );
 }
